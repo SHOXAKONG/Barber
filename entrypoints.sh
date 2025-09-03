0bbc5e923363dc3974d87df72045d5577ce1ea97
Your SHOXAKONG <bekmurodovshohruh0224@gmail.com>
2025-09-03 18:32:15 +0500

    Updated

--- entrypoints.sh
+++ entrypoints.sh
@@ -3,9 +3,7 @@ set -e
 
 echo "🔃 Waiting for PostgreSQL to be available..."
 python << END
-import time
-import socket
-
+import time, socket
 s = socket.socket()
 while True:
     try:
@@ -17,17 +15,17 @@ while True:
         time.sleep(2)
 END
 
-echo "Running Migrations"
-python manage.py migrate
+if [ "$SERVICE" = "web" ]; then
+    echo "Running Migrations"
+    python manage.py migrate
 
-echo "Checking for superuser..."
-python manage.py shell <<EOF
+    echo "Checking for superuser..."
+    python manage.py shell <<EOF
 from decouple import config
 from django.contrib.auth import get_user_model
 from django.core.exceptions import ImproperlyConfigured
 
 User = get_user_model()
-
 phone_number = config('DJANGO_SUPERUSER_PHONE_NUMBER', default='admin')
 
 try:
@@ -39,16 +37,26 @@ if not User.objects.filter(phone_number=phone_number).exists():
     if not password:
         print("DJANGO_SUPERUSER_PASSWORD not found in environment or .env file. Cannot create superuser.")
         exit(1)
-
     print(f"Creating superuser for phone_number '{phone_number}'")
     User.objects.create_superuser(phone_number=phone_number, password=password)
     print("Superuser created successfully.")
 else:
-    print(f"Superuser with email '{phone_number}' already exists. Skipping.")
+    print(f"Superuser with phone_number '{phone_number}' already exists. Skipping.")
 EOF
 
-echo "Collecting static files"
-python manage.py collectstatic --noinput
+    echo "Collecting static files"
+    python manage.py collectstatic --noinput
 
-echo "Start Gunicorn"
-exec gunicorn src.config.wsgi:application --bind 0.0.0.0:8000 --workers 3
+    echo "Start Gunicorn"
+    exec gunicorn src.config.wsgi:application --bind 0.0.0.0:8000 --workers 3
+fi
+
+if [ "$SERVICE" = "celery" ]; then
+    echo "Starting Celery Worker"
+    exec celery -A src.config worker -l info
+fi
+
+if [ "$SERVICE" = "beat" ]; then
+    echo "Starting Celery Beat"
+    exec celery -A src.config beat -l info --scheduler django_celery_beat.schedulers:DatabaseScheduler
+fi
